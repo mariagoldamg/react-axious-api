00a30740cee4983651415b2c04391e4cf172beab
mariagoldamg <mariamelnikova88@gmail.com>
2023-06-16 11:34:48 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+/*
+state = {
+  users:[]
+}
+
+//There are methods get/post/delete:
+
+componentDidMount(){
+axios.get('https://jsonplaceholder.typicode.com/users')
+.then(res=>{
+  const users = res.data;
+  this.setState({users});
+  //console.log(res.data)
+})
+}
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{this.state.users.map((user, id)=> 
+<p key={id}>{user.name} 
+<span> {user.email}</span>
+<span> {user.address.city}</span>
+<span> {user.address.street}</span>
+</p>)}
     </div>
   );
 }
+}
 
+*/
+
+state = {
+  posts:[]
+}
+
+componentDidMount(){
+  axios.get('https://jsonplaceholder.typicode.com/posts ')
+  .then(res=>{
+  const posts = res.data;
+  this.setState({posts})
+
+  
+  })
+}
+
+render(){
+  return(
+    <div>
+{this.state.posts.map((post,id)=>
+
+  <h1 key={id}>{post.title}
+  <span>{post.body}</span></h1>
+
+)}
+
+    </div>
+  )
+}
+}
 export default App;
+
+
+//https://jsonplaceholder.typicode.com/users -  USERS API
+//https://jsonplaceholder.typicode.com/posts  - posts API
